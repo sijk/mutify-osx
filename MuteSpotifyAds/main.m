617ceed8869b948575e23346875f061bc82de228
Simon Knopp <simon.knopp@pg.canterbury.ac.nz>
2013-02-06 12:14:30 +1300

    Initial Commit

---
+++ MuteSpotifyAds/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MuteSpotifyAds
+//
+//  Created by Simon Knopp on 6/02/13.
+//  Copyright (c) 2013 Simon Knopp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
