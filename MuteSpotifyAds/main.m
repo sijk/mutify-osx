b6fd38f49a1ec674be983c467658a4c7a22e3b56
Simon Knopp <simon.knopp@pg.canterbury.ac.nz>
2013-02-06 12:36:28 +1300

    Begin implementing SpotifyObserver

--- MuteSpotifyAds/main.m
+++ MuteSpotifyAds/main.m
@@ -8,15 +8,70 @@
 
 #import <Foundation/Foundation.h>
 
+
+@interface SpotifyObserver : NSObject
+
+- (void)observeStateChange:(NSNotification *)note;
+
+@end
+
+
+@implementation SpotifyObserver
+
+- (id)init
+{
+    if (self = [super init]) {
+        NSNotificationCenter* nc = [NSDistributedNotificationCenter defaultCenter];
+        [nc addObserver:self
+               selector:@selector(observeStateChange:)
+                   name:@"com.spotify.client.PlaybackStateChanged"
+                 object:nil];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [[NSDistributedNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
+}
+
+- (void)observeStateChange:(NSNotification *)note
+{
+    NSString *album  =  [note.userInfo valueForKey:@"Album"];
+    int discNum  = (int)[note.userInfo valueForKey:@"Disc Number"];
+    int trackNum = (int)[note.userInfo valueForKey:@"Track Number"];
+    
+    NSLog(@"Got notification: %@", note.userInfo);
+    
+    bool isAd = NO;
+    if ([album hasPrefix:@"http"]) {
+        isAd = YES;
+        NSLog(@"Album prefix = http (%@)", album);
+    }
+    if ([album hasPrefix:@"spotify"]) {
+        isAd = YES;
+        NSLog(@"Album prefix = spotify (%@)", album);
+    }
+    if (trackNum == 0 && discNum == 0) {
+        isAd = YES;
+        NSLog(@"Track and disc numbers = 0");
+    }
+    
+    if (isAd) {
+        NSLog(@"Hah! Caught an ad.");
+        // Mute spotify...
+    }
+}
+
+@end
+
+
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        [SpotifyObserver new];
+        CFRunLoopRun();
     }
     return 0;
 }
-
